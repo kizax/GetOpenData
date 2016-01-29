a3795a017619a4b6f55e60e9fcae47291d31929c
kizax <kizax>
2016-01-29 20:40:06 +0800

    Get data element initially

--- src/getopendata/GetOpenData.java
+++ src/getopendata/GetOpenData.java
@@ -6,9 +6,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -27,27 +29,75 @@ public class GetOpenData {
 
             StringBuilder inputStringBuilder = new StringBuilder();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gzipInputStream, "UTF-8"));
-            
+
+            String xmlStr = "";
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 inputStringBuilder.append(line);
                 inputStringBuilder.append('\n');
+                xmlStr += (line + '\n');
             }
+
             System.out.println(inputStringBuilder.toString());
 
-//            URL url = new URL(urlString);
-//
-//            URLConnection conn = url.openConnection();
-//
-//            DocumentBuilderFactory factory
-//                    = DocumentBuilderFactory.newInstance();
-//            DocumentBuilder builder = factory.newDocumentBuilder();
-//
-//            Document doc = builder.parse(conn.getInputStream());
-//
-//            Element root = doc.getDocumentElement();
+            DocumentBuilderFactory factory
+                    = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document doc = builder.parse(new StringBufferInputStream(xmlStr));
+
+            NodeList list = doc.getElementsByTagName("VDDevice");
+
+            int a = list.getLength();
+
+            System.out.println(a);
+
+            for (int nodeCount = 0; nodeCount < list.getLength(); nodeCount++) {
+
+                Node nNode = list.item(nodeCount);
+
+                // need to handle many lane data
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("DeviceID: " + eElement.getElementsByTagName("DeviceID")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("TimeInterval: " + eElement.getElementsByTagName("TimeInterval")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("TotalOfLane: " + eElement.getElementsByTagName("TotalOfLane")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("LaneNO" + eElement.getElementsByTagName("LaneNO")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Volume: " + eElement.getElementsByTagName("Volume")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("AvgSpeed: " + eElement.getElementsByTagName("AvgSpeed")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("AvgOccupancy: " + eElement.getElementsByTagName("AvgOccupancy")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Svolume: " + eElement.getElementsByTagName("Svolume")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Mvolume: " + eElement.getElementsByTagName("Mvolume")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Lvolume: " + eElement.getElementsByTagName("Lvolume")
+                            .item(0)
+                            .getTextContent());
+
+                }
+            }
 
         } catch (IOException ex) {
+            Logger.getLogger(GetOpenData.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
             Logger.getLogger(GetOpenData.class.getName()).log(Level.SEVERE, null, ex);
         }
 
