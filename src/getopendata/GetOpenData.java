1e52c557b5e132b50fdc1b7e5919b56088778f6c
kizax <kizax>
2016-01-29 19:24:53 +0800

    First commit, grab xml data from web

---
+++ src/getopendata/GetOpenData.java
@@ -0,0 +1,58 @@
+package getopendata;
+
+import javax.xml.parsers.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ *
+ * @author hunter
+ */
+public class GetOpenData {
+
+    public static void main(String[] args) throws ParserConfigurationException {
+
+        try {
+            String url = "http://data.taipei/tisv/VDDATA";
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet(url);
+            HttpResponse response = httpClient.execute(httpget);
+
+            InputStream inputStream = response.getEntity().getContent();
+            GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+
+            StringBuilder inputStringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gzipInputStream, "UTF-8"));
+            
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                inputStringBuilder.append(line);
+                inputStringBuilder.append('\n');
+            }
+            System.out.println(inputStringBuilder.toString());
+
+//            URL url = new URL(urlString);
+//
+//            URLConnection conn = url.openConnection();
+//
+//            DocumentBuilderFactory factory
+//                    = DocumentBuilderFactory.newInstance();
+//            DocumentBuilder builder = factory.newDocumentBuilder();
+//
+//            Document doc = builder.parse(conn.getInputStream());
+//
+//            Element root = doc.getDocumentElement();
+
+        } catch (IOException ex) {
+            Logger.getLogger(GetOpenData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
